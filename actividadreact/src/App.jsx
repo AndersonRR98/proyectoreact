66a2327a62f50069d12f74e07bd4204a34105127
Anderson David <andersonramosg1995@gmail.com>
2025-05-21 17:47:31 -0500

    avance

---
+++ actividadreact/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Title text="Lista de Usuarios" />
+        <UserList />
+      </Container>
+      <Footer />
+    </>
+   
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ actividadreact/src/Components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header style={{ background: "#282c34", padding: "10px", color: "white" }}>
+      <h2>Mi Aplicación de Usuarios</h2>
+    </header>
+  );
+}
+export default Header;
